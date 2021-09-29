b38806529ea0cba16def19238600680f6b0dcc28
Darshit Sarna <op.dare7@gmail.com>
2021-09-29 16:32:13 +0530

    added source code link with icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import { FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
     return (
@@ -8,7 +9,7 @@ const Footer = () => {
                 <Col className='text-center py-3'>
                     Made with ❤️ by Darshit Singh
                     <br />
-                    <a href="https://github.com/darshit-singh/algorithm-visualizer-react" target="_blank" rel="noopener noreferrer"> Source Code</a>
+                    <a href="https://github.com/darshit-singh/algorithm-visualizer-react" target="_blank" rel="noopener noreferrer"> <FaGithub style={{ margin: '0 5px 2px 0' }} />Source Code</a>
                 </Col>
             </Row>
         </Container>
