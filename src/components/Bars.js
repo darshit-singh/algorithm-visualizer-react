323632d624cacea90e840955d8b103c4560f6442
Darshit Sarna <op.dare7@gmail.com>
2021-10-05 00:15:34 +0530

    multiple bars made

--- src/components/Bars.js
+++ src/components/Bars.js
@@ -4,12 +4,13 @@ const Bars = () => {
     let bg = '#00bc8c'; // sorted color
     const styles = {
         arrayContainer: {
-            paddingTop: '5rem',
             minHeight: '60vh',
-            position: 'relative'
+            display: 'flex',
+            alignItems: 'flex-end',
+            justifyContent: 'center'
         }
     }
-    let blocks = [1, 3, 5, 6, 7, 7, 87, 6];
+    let blocks = [1, 3, 5, 6, 7, 7, 4, 6];
     return (
         <div className='arrayContainer' style={styles.arrayContainer}>
             {blocks.map(bar => {
@@ -18,14 +19,14 @@ const Bars = () => {
                         backgroundColor: bg,
                         // color: color,
                         width: '1rem',
-                        height: '10rem',
-                        position: 'absolute',
-                        bottom: 0
+                        height: `${bar}rem`,
+                        bottom: 0,
+                        margin: '0px 2px',
                     }
                 }
                 return (
                     <div className='bar' style={styles.bar}>
-
+                        <span>{bar}</span>
                     </div>
                 )
             })}
