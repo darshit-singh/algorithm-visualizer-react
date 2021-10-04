78837a59e1dcae8693f04a666f189036f2acaaef
Darshit Sarna <op.dare7@gmail.com>
2021-10-04 23:57:45 +0530

    made a block

--- src/components/Bars.js
+++ src/components/Bars.js
@@ -1,19 +1,36 @@
 import React from 'react'
 
 const Bars = () => {
+    let bg = '#00bc8c'; // sorted color
+    const styles = {
+        arrayContainer: {
+            paddingTop: '5rem',
+            minHeight: '60vh',
+            position: 'relative'
+        }
+    }
+    let blocks = [1, 3, 5, 6, 7, 7, 87, 6];
     return (
         <div className='arrayContainer' style={styles.arrayContainer}>
-            ajaj
+            {blocks.map(bar => {
+                const styles = {
+                    bar: {
+                        backgroundColor: bg,
+                        // color: color,
+                        width: '1rem',
+                        height: '10rem',
+                        position: 'absolute',
+                        bottom: 0
+                    }
+                }
+                return (
+                    <div className='bar' style={styles.bar}>
+
+                    </div>
+                )
+            })}
         </div>
     )
-}
-
-const styles = {
-    arrayContainer: {
-        paddingTop: '5rem',
-        minHeight: '78vh',
-        backgroundColor: '#aaa'
-    }
 }
 
 export default Bars
